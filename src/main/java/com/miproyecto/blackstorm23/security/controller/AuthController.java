a58f1e7c744bf91da4379149eabaf61e319a0a39
Blackstorm23 <soyelegido15@gmail.com>
2023-01-12 19:38:56 -0300

    uniendo al front

--- src/main/java/com/miproyecto/blackstorm23/security/controller/AuthController.java
+++ src/main/java/com/miproyecto/blackstorm23/security/controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = {"https://porfolio-black-5401c.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
